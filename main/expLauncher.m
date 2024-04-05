51801b504fcb3813ad2022c7ad3148b451cfbb96
Martin Szinte <martin.szinte@gmail.com>
2024-04-05 14:34:42 +0200

    sub-01

--- main/expLauncher.m
+++ main/expLauncher.m
@@ -34,16 +34,16 @@ Screen('CloseAll'); clear all; clear mex; clear functions; close all; ...
 
 % General settings
 const.expName = 'DoubleDrift';          % experiment name
-const.expStart = 0;                     % Start of a recording (0 = NO, 1 = YES)
+const.expStart = 1;                     % Start of a recording (0 = NO, 1 = YES)
 const.checkTrial = 0;                   % Print trial conditions (0 = NO, 1 = YES)
-const.mkVideo = 1;                      % Make a video (0 = NO, 1 = YES)
+const.mkVideo = 0;                      % Make a video (0 = NO, 1 = YES)
 
 % External controls
-const.tracker = 0;                      % run with eye tracker (0 = NO, 1 = YES)
+const.tracker = 1;                      % run with eye tracker (0 = NO, 1 = YES)
 const.display = 1;                      % diplay (1 = Display++; 2 = MacBookPro)
 
 % Desired screen settings
-const.desiredFD = 30;                  % Desired refresh rate
+const.desiredFD = 120;                  % Desired refresh rate
 const.desiredRes = [1920, 1080];        % Desired resolution
 
 % Path
